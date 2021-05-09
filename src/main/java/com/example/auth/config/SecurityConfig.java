2ee0df8f3121c5a0472f2a048ec2eb0923ca7b45
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2021-05-09 18:39:05 -0300

    roles

--- src/main/java/com/example/auth/config/SecurityConfig.java
+++ src/main/java/com/example/auth/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -21,13 +22,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("user")
-                .password(passwordEncoder().encode("password")).authorities("USER");
+                .password(passwordEncoder().encode("password")).authorities("USER")
+        .and().withUser("admin").password(passwordEncoder().encode("admin"))
+        .authorities("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().formLogin()
-                .loginPage("/login").permitAll();
+        http.authorizeRequests()
+                .antMatchers("/").access("hasAnyAuthority('USERS', 'ADMIN')")
+                .antMatchers("/admin").access("hasAuthority('ADMIN')")
+                .anyRequest().authenticated().and().formLogin()
+                .loginPage("/login").permitAll()
+        .and()
+        .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+        .logoutSuccessUrl("/login").permitAll();
     }
 
     @Override
